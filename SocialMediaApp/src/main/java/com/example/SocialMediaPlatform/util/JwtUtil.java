91547f4482dce1cf16a408b937914addad9769ff
arsalan99999 <arslanishtiaq70@gmail.com>
2025-04-18 22:02:47 +0500

    Added jwt expiry

--- SocialMediaApp/src/main/java/com/example/SocialMediaPlatform/util/JwtUtil.java
+++ SocialMediaApp/src/main/java/com/example/SocialMediaPlatform/util/JwtUtil.java
@@ -5,10 +5,12 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.ObjectInputFilter;
 import java.security.Key;
+import java.util.Date;
 
 @Component
 public class JwtUtil {
@@ -16,11 +18,15 @@ public class JwtUtil {
     @Autowired
     private Key key;
 
+    @Value("${jwt-expiry}")
+    private long jwtExpiry;
+
     public String generateToken(String email, int id){
 
         return Jwts.builder()
                 .setSubject(email)
                 .claim("id",id)
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiry))
                 .signWith(key, SignatureAlgorithm.HS512)
                 .compact();
 
